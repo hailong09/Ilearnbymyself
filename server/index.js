ec006036727fd4320c678624153eab16e352ce4a
unknown <long2qw@gmail.com>
2021-05-05 18:02:41 -0400

    done

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 const authRoutes = require('./routes/auth')
 const postRoutes = require('./routes/post');
 const PORT = 8080 || process.env.PORT;
